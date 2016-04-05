24401c008ef5cf61f1d9332055ca4d77faf64834
Hendrik Lievens <hendrik.lievens@student.uhasselt.be>
2016-04-05 16:26:29 +0200

    Add DB example.

---
+++ modulo-backend/src/main/java/be/lambdaware/dao/Student.java
@@ -0,0 +1,31 @@
+package be.lambdaware.dao;
+
+/**
+ * Created by hendrik on 05/04/16.
+ */
+public class Student {
+    private Integer age;
+    private String name;
+    private Integer id;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+}

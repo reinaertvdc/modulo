d93899e706234f962c73cabacc092677c8f4a389
Hendrik Lievens <hendrik.lievens@student.uhasselt.be>
2016-04-05 14:51:26 +0200

    Add clean modulo-backend project.

---
+++ modulo-backend/src/main/java/be/lambdaware/ModuloBackendApplication.java
@@ -0,0 +1,12 @@
+package be.lambdaware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloBackendApplication.class, args);
+	}
+}

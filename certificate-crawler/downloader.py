4c5d1a12638ffe0c13df3d043c75c66c4d972417
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2016-04-15 00:25:02 +0200

    Implement downloading all certificates and parsing them to plain text.

--- certificate-crawler/downloader.py
+++ certificate-crawler/downloader.py
@@ -1,3 +1,5 @@
+import os
+import urllib
 import urllib2
 
 
@@ -5,6 +7,7 @@ class Downloader:
     __CERTIFICATE_WEB_PAGE_URL = \
         'http://www.ond.vlaanderen.be/curriculum/leren-en-werken/deeltijds-beroepssecundair-onderwijs/opleidingen/'
     __ABSOLUTE_URL_START_TAG = 'http://'
+    __CERTIFICATE_POSTFIX = '.pdf'
 
     def __init__(self):
         pass
@@ -12,8 +15,20 @@ class Downloader:
     def get_certificate_web_page(self):
         return urllib2.urlopen(self.__CERTIFICATE_WEB_PAGE_URL).read()
 
-    def get_certificate_pdf(self, url):
-        if url.startswith(self.__ABSOLUTE_URL_START_TAG):
-            return urllib2.urlopen(url).read()
-        else:
-            return urllib2.urlopen(self.__CERTIFICATE_WEB_PAGE_URL + url).read()
+    def __save_certificate_pdf(self, url, save_path):
+        if not os.path.exists(os.path.dirname(save_path)):
+            os.makedirs(os.path.dirname(save_path))
+        try:
+            testfile = urllib.URLopener()
+            if url.startswith(self.__ABSOLUTE_URL_START_TAG):
+                testfile.retrieve(url, save_path)
+            else:
+                testfile.retrieve(self.__CERTIFICATE_WEB_PAGE_URL + url, save_path)
+        except:
+            pass
+
+    def save_certificate_pdfs(self, urls, save_path):
+        index = 1
+        for url in urls:
+            self.__save_certificate_pdf(url, save_path + str(index) + self.__CERTIFICATE_POSTFIX)
+            index += 1
--- certificate-crawler/main.py
+++ certificate-crawler/main.py
@@ -6,17 +6,23 @@ SECTION_MODULAR_PROF_CERTIFICATES_TITLE = 'Modulair georganiseerd op basis van e
 SECTION_MODULAR_CERTIFICATES_TITLE = 'Modulair georganiseerd'
 SECTION_LINEAR_CERTIFICATES_TITLE = 'Nog niet omgeschakelde rubrieken met bestaande lineaire opleidingen'
 
+CERTIFICATES_DIR = 'certificates/'
+MODULAR_PROF_CERTIFICATES_DIR = 'modular_prof/'
+MODULAR_CERTIFICATES_DIR = 'modular/'
+LINEAR_CERTIFICATES_DIR = 'linear/'
+
 # get the certificates web page
 downloader = Downloader()
 web_page_parser = WebPageParser(downloader.get_certificate_web_page())
 
 # list of certificates modularly organised based on professional qualification
-modular_prof_certificate_urls = web_page_parser.get_certificate_pdf_urls(SECTION_MODULAR_PROF_CERTIFICATES_TITLE)
+downloader.save_certificate_pdfs(web_page_parser.get_certificate_pdf_urls(SECTION_MODULAR_PROF_CERTIFICATES_TITLE),
+                                 CERTIFICATES_DIR + MODULAR_PROF_CERTIFICATES_DIR)
 
 # list of certificates modularly organised
-modular_certificate_urls = web_page_parser.get_certificate_pdf_urls(SECTION_MODULAR_CERTIFICATES_TITLE)
+downloader.save_certificate_pdfs(web_page_parser.get_certificate_pdf_urls(SECTION_MODULAR_CERTIFICATES_TITLE),
+                                 CERTIFICATES_DIR + MODULAR_CERTIFICATES_DIR)
 
 # list of yet unconverted certificates with existing linear courses
-linear_certificate_urls = web_page_parser.get_certificate_pdf_urls(SECTION_LINEAR_CERTIFICATES_TITLE)
-
-#print downloader.get_certificate_pdf(modular_prof_certificate_urls[0])
+downloader.save_certificate_pdfs(web_page_parser.get_certificate_pdf_urls(SECTION_LINEAR_CERTIFICATES_TITLE),
+                                 CERTIFICATES_DIR + LINEAR_CERTIFICATES_DIR)

224772d658e2db7c3ca7060f8b8e23e71ff2166b
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2016-04-14 22:37:35 +0200

    Implement getting and downloading list of certificate pdf's in certificate crawler.

--- certificate-crawler/downloader.py
+++ certificate-crawler/downloader.py
@@ -2,14 +2,18 @@ import urllib2
 
 
 class Downloader:
-    __CERTIFICATE_PAGE_URL = \
+    __CERTIFICATE_WEB_PAGE_URL = \
         'http://www.ond.vlaanderen.be/curriculum/leren-en-werken/deeltijds-beroepssecundair-onderwijs/opleidingen/'
+    __ABSOLUTE_URL_START_TAG = 'http://'
 
     def __init__(self):
         pass
 
-    def get_certificate_page(self):
-        return urllib2.urlopen(self.__CERTIFICATE_PAGE_URL).read()
+    def get_certificate_web_page(self):
+        return urllib2.urlopen(self.__CERTIFICATE_WEB_PAGE_URL).read()
 
-    def get_certificate_pdf(self, download_path):
-        return urllib2.urlopen(self.__CERTIFICATE_PAGE_URL + download_path).read()
+    def get_certificate_pdf(self, url):
+        if url.startswith(self.__ABSOLUTE_URL_START_TAG):
+            return urllib2.urlopen(url).read()
+        else:
+            return urllib2.urlopen(self.__CERTIFICATE_WEB_PAGE_URL + url).read()
--- certificate-crawler/main.py
+++ certificate-crawler/main.py
@@ -1,9 +1,22 @@
-# certificates modularly organised based on professional qualification
-modular_prof_certificate_urls = []
+from downloader import Downloader
+from web_page_parser import WebPageParser
 
-# certificates modularly organised
-modular_certificate_urls = []
+# titles of the different sections in the certificates web page
+SECTION_MODULAR_PROF_CERTIFICATES_TITLE = 'Modulair georganiseerd op basis van een beroepskwalificatie'
+SECTION_MODULAR_CERTIFICATES_TITLE = 'Modulair georganiseerd'
+SECTION_LINEAR_CERTIFICATES_TITLE = 'Nog niet omgeschakelde rubrieken met bestaande lineaire opleidingen'
 
-# yet unconverted certificates with existing linear courses
-linear_certificate_urls = []
+# get the certificates web page
+downloader = Downloader()
+web_page_parser = WebPageParser(downloader.get_certificate_web_page())
 
+# list of certificates modularly organised based on professional qualification
+modular_prof_certificate_urls = web_page_parser.get_certificate_pdf_urls(SECTION_MODULAR_PROF_CERTIFICATES_TITLE)
+
+# list of certificates modularly organised
+modular_certificate_urls = web_page_parser.get_certificate_pdf_urls(SECTION_MODULAR_CERTIFICATES_TITLE)
+
+# list of yet unconverted certificates with existing linear courses
+linear_certificate_urls = web_page_parser.get_certificate_pdf_urls(SECTION_LINEAR_CERTIFICATES_TITLE)
+
+#print downloader.get_certificate_pdf(modular_prof_certificate_urls[0])

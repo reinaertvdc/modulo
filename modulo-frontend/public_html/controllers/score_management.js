e02487ac07fe5c2676f4139a4ee632f8791a5f6b
Jens Vannitsen <jens.vannitsen@student.uhasselt.be>
2016-04-15 15:50:14 +0100

    Add first draft score_management frontend.

--- modulo-frontend/public_html/controllers/score_management.js
+++ modulo-frontend/public_html/controllers/score_management.js
@@ -1,3 +1,133 @@
 app.controller('ScoreManagementController', function ($scope) {
     // TODO implement controller
 });
+
+app.controller('ExampleController', ['$scope', function($scope) {
+    $scope.data = {
+        singleSelect: null,
+        multipleSelect: [],
+        option1: 'option-1',
+    };
+}]);
+
+/*
+app.controller('MyCtrl', function($scope) {
+    $scope.mySelections = [];
+
+    $scope.myData = [{name: "Moroni", id: 1},
+        {name: "Tiancum", id: 2},
+        {name: "Jacob", id: 3},
+        {name: "Nephi", id: 4},
+        {name: "Akon", id: 5},
+        {name: "Enos", id: 6}];
+
+    $scope.gridOptions = {
+        data: 'myData',
+        selectedItems: $scope.mySelections,
+        multiSelect: true,
+        enableCellSelection: true,
+        enableRowSelection: false,
+        enableCellEdit: false,
+        afterSelectionChange: function () {
+            $scope.selectedIDs = [];
+
+        }
+    };
+
+});*/
+
+app.controller('DatepickerPopupDemoCtrl', function ($scope) {
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function() {
+        $scope.dt = null;
+    };
+
+    $scope.inlineOptions = {
+        customClass: getDayClass,
+        minDate: new Date(),
+        showWeeks: true
+    };
+
+    $scope.dateOptions = {
+        dateDisabled: disabled,
+        formatYear: 'yy',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(),
+        startingDay: 1
+    };
+
+    // Disable weekend selection
+    function disabled(data) {
+        var date = data.date,
+            mode = data.mode;
+        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    }
+
+    $scope.toggleMin = function() {
+        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+    };
+
+    $scope.toggleMin();
+
+    $scope.open1 = function() {
+        $scope.popup1.opened = true;
+    };
+
+    $scope.open2 = function() {
+        $scope.popup2.opened = true;
+    };
+
+    $scope.setDate = function(year, month, day) {
+        $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.popup1 = {
+        opened: false
+    };
+
+    $scope.popup2 = {
+        opened: false
+    };
+
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 1);
+    $scope.events = [
+        {
+            date: tomorrow,
+            status: 'full'
+        },
+        {
+            date: afterTomorrow,
+            status: 'partially'
+        }
+    ];
+
+    function getDayClass(data) {
+        var date = data.date,
+            mode = data.mode;
+        if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+            for (var i = 0; i < $scope.events.length; i++) {
+                var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                if (dayToCheck === currentDay) {
+                    return $scope.events[i].status;
+                }
+            }
+        }
+
+        return '';
+    }
+});

2a60c2383011f7b18f194eb179e003629385dd5f
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2016-04-15 11:00:40 +0200

    Move web frontend dummy data to separate folder, add function to get certificates.

---
+++ modulo-frontend/public_html/js/dummy_data/certificates.js
@@ -0,0 +1,43 @@
+backend.certificates = [
+    new Certificate(0, 'Metselaar', true, [
+        new SubCertificate(0, 'Basistechnieken metselwerk', null, [
+            new SubCertificateCategory(0, 'veilig, hygiënisch en milieubewust werken conform welzijn op het werk en de geldende regelgevingen', null, [
+                new Competence(0, 'ergonomisch werken', null),
+                new Competence(1, 'persoonlijke beschermingsmiddelen gebruiken', null),
+                new Competence(2, 'collectieve beschermingsmiddelen aanbrengen', null),
+                new Competence(3, 'producten met gevaarlijke eigenschappen correct gebruiken', null),
+                new Competence(4, 'veiligheidsvoorschriften en -instructies inzake arbeidsmiddelen toepassen', null),
+                new Competence(5, 'economisch werken', null),
+                new Competence(6, 'afval en restproducten sorteren', null),
+                new Competence(7, 'gereedschap en machines gebruiken', null),
+                new Competence(8, 'gereedschap en machines reinigen', null)
+            ]),
+            new SubCertificateCategory(1, 'noodzakelijke houdingen voor de uitoefening van het beroep aannemen', null, [
+                new Competence(9, 'met zin voor precisie werken', null),
+                new Competence(10, 'zin voor samenwerking tonen', null),
+                new Competence(11, ' doorzettingsvermogen tonen', null)
+            ]),
+            new SubCertificateCategory(2, 'functionele vaardigheden voor de uitoefening van het beroep toepassen', null, [
+                new Competence(12, 'dimensies (lengte, breedte, dikte, oppervlakte, inhoud, …) meten en berekenen', null),
+                new Competence(13, 'meetinstrumenten gebruiken', null),
+                new Competence(14, 'technische tekening gebruiken', null)
+            ]),
+            new SubCertificateCategory(3, 'eigen werkzaamheden organiseren', null, [
+                new Competence(15, 'eigen werkzaamheden voorbereiden', null),
+                new Competence(16, 'eigen werkzaamheden uitvoeren', null),
+                new Competence(17, 'eigen werkzaamheden evalueren', null),
+                new Competence(18, 'eigen werkzaamheden bijsturen', null),
+            ]),
+            new SubCertificateCategory(4, 'basistechnieken van eenvoudig metselwerk uitvoeren', null, [
+                new Competence(19, 'mortel aanmaken', null),
+                new Competence(20, 'halfsteense muren metselen', null),
+                new Competence(21, 'éénsteense muren metselen', null),
+                new Competence(22, 'kleine elementen in metselwerk plaatsen', null),
+                new Competence(23, 'voegen uitkrabben', null),
+                new Competence(24, 'meegaand voegen', null),
+                new Competence(25, '', null),
+                new Competence(26, '', null)
+            ])
+        ])
+    ])
+];

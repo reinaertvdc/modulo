bbda7cf90f15d349c1b38e56491467beeef6bbf3
Jens Vannitsen <jens.vannitsen@student.uhasselt.be>
2016-04-22 11:31:33 +0100

    Add backend connection studentlist and coursetopiclist in manage_class.

--- modulo-frontend/public_html/controllers/manage_class_course_topic_list.js
+++ modulo-frontend/public_html/controllers/manage_class_course_topic_list.js
@@ -1,8 +1,50 @@
 app.controller('ManageClassCourseTopicListController', function ($scope) {
-    const MANAGE_CLASS_LIST_ITEM_PREFIX = 'manage-class-list-item-';
-    const COURSE_TOPIC_LIST_ELEMENT = document.getElementById('manage-class-topic-list-body');
-    const COURSE_TOPIC_LIST_HEADER_ELEMENT = document.getElementById('manage-class-topic-list-header');
+    const COURSE_TOPIC_LIST_ITEM_PREFIX = 'manage-course-topic-list-item-';
+    const COURSE_TOPIC_LIST_ELEMENT = document.getElementById('manage-course-topic-list-body');
 
     $scope.courseTopics = new Map();
+
+    $scope.toElementId = function (id) {
+        return COURSE_TOPIC_LIST_ITEM_PREFIX + id;
+    };
+
+    $scope.addCourseTopic = function (courseTopic) {
+        $scope.removeClassFrontend(courseTopic.id);
+        $scope.courseTopics.set(courseTopic.id, courseTopic);
+
+        var html = '<tr id="' + $scope.toElementId(addclass.id) + '">' +
+            '<td>' + courseTopic.name + '</td>' +
+            '<td class="text-info"  ng-click="location.setParameter(location.PARAM_MANAGE_COURSE_TOPIC_ID,'+courseTopic.id+')"><span role="button" class="glyphicon glyphicon-edit"></span></td>' +
+            '<td class="text-danger" ng-click="removeCourseTopicBackend(' +  courseTopic.id + ')"><span role="button" class="glyphicon glyphicon-remove"></span></td>' +
+            '</tr>';
+
+        var element = document.createElement('tr');
+        element.outerHTML = html;
+
+    };
+
+    $scope.removeCourseTopicBackend = function (id) {
+        $scope.removeCourseTopicFrontend(id);
+        //$http.delete('http://localhost:8080/class/'+id);
+    };
+
+    $scope.removeCourseTopicFrontend = function (id) {
+        $scope.classes.delete(id);
+        var element = document.getElementById($scope.toElementId(id));
+        if (element !== null)
+            element.parentElement.removeChild(element);
+    }
+
+    // Update the Angular controls that have been added in the HTML
+    $scope.refresh = function () {
+        $compile(COURSE_TOPIC_LIST_ELEMENT)($scope);
+    };
+
+   /* $http.get('http://localhost:8080/class/teacher/' + $scope.backend.getUser().id).success(function (response) {
+        response.forEach(function (item) {
+            $scope.addCourseTopic(item.classEntity)
+        });
+        $scope.refresh();
+    });*/
     
 });

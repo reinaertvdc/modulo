2f75d4f3c8a03935718c1d2cf3c75612eb3c2279
1131102 <martijn.maes@student.uhasselt.be>
2016-04-28 15:08:53 +0200

    manage_class_details working with db connection

--- modulo-frontend/public_html/controllers/manage_class_details.js
+++ modulo-frontend/public_html/controllers/manage_class_details.js
@@ -1,3 +1,25 @@
-app.controller('ManageClassDetailsController', function ($scope) {
+app.controller('ManageClassDetailsController', function ($scope, $http) {
     // TODO implement controller
+    $scope.className = document.getElementById('className');
+    $scope.classId = $scope.location.getParameter($scope.location.PARAM_MANAGE_CLASS_ID);
+
+
+    $scope.saveDetails = function(){
+        $scope.class.name =  $scope.className.value;
+        var model = JSON.stringify({"classEntity": $scope.class});
+
+        $http.put('http://localhost:8080/class', model).success(function (response) {
+            if(response){
+                console.log("PUT succes");
+            }
+            else {
+                console.log("PUT failed");
+            }
+        });
+    }
+
+    $http.get('http://localhost:8080/class/' + $scope.classId).success(function (response) {
+        $scope.class = response.classEntity;
+        $scope.className.value = $scope.class.name;
+    });
 });

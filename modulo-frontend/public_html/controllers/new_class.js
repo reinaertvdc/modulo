e6d63044971e294371b386fe570a00ccab898225
Jens Vannitsen <jens.vannitsen@student.uhasselt.be>
2016-04-28 13:34:12 +0100

    Add input field and dropdown.

--- modulo-frontend/public_html/controllers/new_class.js
+++ modulo-frontend/public_html/controllers/new_class.js
@@ -76,5 +76,28 @@ app.controller('NewClassController', function ($scope, $http) {
         //Expand all
         $scope.checkableTree.treeview('expandAll', {silent: true});
     }
+});
 
+app.controller('DropdownCtrl', function ($scope, $log) {
+    $scope.items = [
+        'The first choice!',
+        'And another choice for you.',
+        'but wait! A third!'
+    ];
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        $log.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
+    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
 });

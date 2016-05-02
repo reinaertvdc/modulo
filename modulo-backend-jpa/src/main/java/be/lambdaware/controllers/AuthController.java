d22069cde8d36593bb3850cdc626635f53529b00
1131102 <martijn.maes@student.uhasselt.be>
2016-05-02 13:01:55 +0200

    Added @CrossOrigin to AuthController

--- modulo-backend-jpa/src/main/java/be/lambdaware/controllers/AuthController.java
+++ modulo-backend-jpa/src/main/java/be/lambdaware/controllers/AuthController.java
@@ -11,10 +11,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * {@link AuthController} is the controller that is mapped to /auth and is used to verify credentials.
@@ -24,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
 
     @Autowired

f2f2cbb07adca2cfffa81764fd2c884b32c1b258
1131102 <martijn.maes@student.uhasselt.be>
2016-05-02 13:01:31 +0200

    Refactor log_in start

--- modulo-frontend/public_html/controllers/log_in.js
+++ modulo-frontend/public_html/controllers/log_in.js
@@ -1,16 +1,36 @@
-app.controller('LogInController', function ($scope, $http) {
+app.controller('LogInController', function ($scope, $http, $base64) {
     // TODO finish controller
     $scope.formData = {};
 
     $scope.submitForm = function () {
-        $http.get('http://localhost:8080/account/email/' + $scope.formData.email + '/').success(function (response) {
-            // console.log(response.userEntity);
+        var auth = $scope.formData.email + ":" + $scope.formData.password;
+        auth = $base64.encode(auth);
+
+        $http.get('http://localhost:8080/auth', {headers: {'X-Auth': auth}}).success(function (response){
+            console.log("Response " + response.email);
+/*
+            // Setting a cookie
+            $cookies.put('auth', auth);
+            // Retrieving a cookie
+            var testCookie = $cookies.get('auth');
+            console.log("Cookie: " + testCookie);
+*/
+        }).error(function (response, code){
+            console.log("Error " + response);
+            console.log("Code " + code);
+        });
+        /*
+        $http.get('http://localhost:8080/auth' + $scope.formData.email + '/').success(function (response) {
+            console.log("TEST" + $http.succes);
+            console.log("TEST1: " + $http.error);
 
             if (response.userEntity.password !== $scope.formData.password)
                 showAlert(AlertType.DANGER, 'Ongeldig e-mailadres/wachtwoord.', 'Het ingegeven e-mailadres en wachtwoord komen niet overeen.', true);
             else
                 $scope.account.user = response.userEntity;
-        });
 
+        });
+*/
     };
+
 });

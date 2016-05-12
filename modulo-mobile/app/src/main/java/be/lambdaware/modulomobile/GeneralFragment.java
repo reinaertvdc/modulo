d3dc1f983e10aba264b941c1418ddb6c8880a307
Hendrik Lievens <hendrik.lievens@student.uhasselt.be>
2016-05-12 16:55:16 +0200

    Added required fragments and layouts. Working mains screen and dummy login.

---
+++ modulo-mobile/app/src/main/java/be/lambdaware/modulomobile/GeneralFragment.java
@@ -0,0 +1,19 @@
+package be.lambdaware.modulomobile;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GeneralFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_general, null);
+    }
+}

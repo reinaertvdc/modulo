2d3e33c5e48530ec1e7c1c8a10e2c53f9d8a02b0
Hendrik Lievens <hendrik.lievens@student.uhasselt.be>
2016-05-19 16:49:55 +0200

    Downloading tasks and children from back-end.

--- modulo-mobile/app/src/main/java/be/lambdaware/modulomobile/api/RestCallback.java
+++ modulo-mobile/app/src/main/java/be/lambdaware/modulomobile/api/RestCallback.java
@@ -1,9 +1,11 @@
 package be.lambdaware.modulomobile.api;
 
+import org.json.JSONException;
+
 /**
  * Created by Hendrik on 13/05/2016.
  */
 public interface RestCallback {
 
-    void onSuccess(String response);
+    void onSuccess(String response) throws JSONException;
 }
--- modulo-mobile/app/src/main/java/be/lambdaware/modulomobile/fragments/ChildSelectFragment.java
+++ modulo-mobile/app/src/main/java/be/lambdaware/modulomobile/fragments/ChildSelectFragment.java
@@ -11,24 +11,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.sql.Date;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 import be.lambdaware.modulomobile.R;
 import be.lambdaware.modulomobile.adapters.ChildListAdapter;
-import be.lambdaware.modulomobile.adapters.TaskListAdapter;
-import be.lambdaware.modulomobile.models.Task;
+import be.lambdaware.modulomobile.api.ApiAuthentication;
+import be.lambdaware.modulomobile.api.RestCall;
+import be.lambdaware.modulomobile.api.RestCallback;
 import be.lambdaware.modulomobile.models.User;
 
 
-public class ChildSelectFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class ChildSelectFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, RestCallback {
 
     // This recyclerview will contain all the scores as cards.
     private RecyclerView rvRecylcerView;
-    private RecyclerView.Adapter scoreAdapter;
+    private RecyclerView.Adapter childAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
     private SwipeRefreshLayout srSwipeRefreshLayout;
+
+    private RestCall restCall;
 
     @Nullable
     @Override
@@ -42,25 +51,48 @@ public class ChildSelectFragment extends Fragment implements SwipeRefreshLayout.
         rvRecylcerView.setLayoutManager(layoutManager);
 
         ArrayList<User> data = new ArrayList<>();
-        data.add(new User(1, "email@vankind.be", "Zoon", "Van ouder", "MALE", "STUDENT", true));
-        scoreAdapter = new ChildListAdapter(getContext(), data);
-        rvRecylcerView.setAdapter(scoreAdapter);
+        childAdapter = new ChildListAdapter(getContext(), data);
+        rvRecylcerView.setAdapter(childAdapter);
 
         srSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.srl_child_refresh_layout);
         srSwipeRefreshLayout.setOnRefreshListener(this);
 
+        srSwipeRefreshLayout.setRefreshing(true);
+        loadChildren();
         return view;
     }
 
     @Override
     public void onRefresh() {
-        Log.i("GeneralFragment", "Refreshing list...");
-        //TODO implement refresh from database.
-        srSwipeRefreshLayout.setRefreshing(false);
+        loadChildren();
     }
 
     @Override
     public String toString() {
         return "TaskFragment";
     }
+
+    @Override
+    public void onSuccess(String response) throws JSONException {
+        srSwipeRefreshLayout.setRefreshing(false);
+
+        JSONArray JSONArray = new JSONArray(response);
+        ArrayList<User> data = new ArrayList<>();
+        for (int i = 0; i < JSONArray.length(); i++) {
+            JSONObject JSONUser = JSONArray.getJSONObject(i);
+            Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+            User user = gson.fromJson(JSONUser.toString(), User.class);
+            data.add(user);
+        }
+
+        childAdapter = new ChildListAdapter(getContext(), data);
+        rvRecylcerView.setAdapter(childAdapter);
+    }
+
+    private void loadChildren() {
+        Log.i("ChildSelectFragment", "Getting al users...");
+        restCall = new RestCall(this);
+        restCall.execute("http://10.0.2.2:8080/user/id/" + ApiAuthentication.getAuthenticatedUser().getId() + "/children");
+    }
+
 }
